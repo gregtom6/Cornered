8d5f3e3c6614b31b8b83b775387f191c9e1e24e6
gregtom6 <gregtom6@gmail.com>
2024-06-15 10:25:41 +0200

    adding fence audio management

--- Cornered/Assets/Scripts/Components/Interactables/CGateController.cs
+++ Cornered/Assets/Scripts/Components/Interactables/CGateController.cs
@@ -8,6 +8,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CAudioPlayer))]
 public class CGateController : MonoBehaviour
 {
     private static readonly int ANIM_PARAM_OPEN = Animator.StringToHash("open");
@@ -15,6 +16,23 @@ public class CGateController : MonoBehaviour
     [SerializeField] private Animator m_GateAnimator;
     [SerializeField] private CGateEnterDetector m_GateEnterDetector;
     [SerializeField] private CGateEnterDetector m_GateExitDetector;
+
+    private CAudioPlayer m_AudioPlayer;
+
+    public void OnGateMovementStarted()
+    {
+        m_AudioPlayer.Play(transform);
+    }
+
+    public void OnGateMovementFinished()
+    {
+        m_AudioPlayer.Stop();
+    }
+
+    private void Start()
+    {
+        m_AudioPlayer = GetComponent<CAudioPlayer>();
+    }
 
     private void OnEnable()
     {
